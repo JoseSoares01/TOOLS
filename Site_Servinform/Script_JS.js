b98cb13001f9292bf8aaf0bcbec2028047624d94
José Soares <janyelrodrigues@hotmail.com>
2024-07-26 20:34:46 +0100

    alteração nas paginas para que fiquem responsivas

--- Site_Servinform/Script_JS.js
+++ Site_Servinform/Script_JS.js
@@ -5,13 +5,13 @@ document.addEventListener('DOMContentLoaded', function() {
         dropdown.addEventListener('mouseover', function() {
             const submenu = this.querySelector('ul');
             if (submenu) {
-                submenu.style.display = 'block';
+                submenu.style.display = 'block'; // Mostra o submenu quando o item é "mouseover"
             }
         });
         dropdown.addEventListener('mouseout', function() {
             const submenu = this.querySelector('ul');
             if (submenu) {
-                submenu.style.display = 'none';
+                submenu.style.display = 'none'; // Esconde o submenu quando o item é "mouseout"
             }
         });
     });
