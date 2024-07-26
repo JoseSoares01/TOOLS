56952c3d24d47300b0a2ff1a0f90c4d732849d2b
José Soares <janyelrodrigues@hotmail.com>
2024-07-26 23:06:07 +0100

    codigo mapeado

--- Site_Servinform/Script_JS.js
+++ Site_Servinform/Script_JS.js
@@ -1,24 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    Script_Js();
+    Script_Js(); // Chama a função Script_Js quando o DOM está completamente carregado
 });
 
 function Script_Js() {
-    // Dropdown menu functionality
-    const menuItems = document.querySelectorAll('nav.menu .menu-item');
-    menuItems.forEach(menuItem => {
-        menuItem.addEventListener('mouseover', function() {
-            const submenu = this.querySelector('.sub-menu');
-            if (submenu) {
-                submenu.style.visibility = 'visible';
-                submenu.style.maxHeight = '12rem';
-            }
+    // Seleciona todos os itens de menu que possuem submenus
+    const dropdowns = document.querySelectorAll('nav.menu .menu-item');
+
+    // Adiciona eventos de mouseover e mouseout para cada item do menu
+    dropdowns.forEach(dropdown => {
+        // Mostra o submenu ao passar o mouse sobre o item
+        dropdown.addEventListener('mouseover', function() {
+            this.querySelector('.sub-menu').style.visibility = 'visible';
+            this.querySelector('.sub-menu').style.maxHeight = '12rem';
         });
-        menuItem.addEventListener('mouseout', function() {
-            const submenu = this.querySelector('.sub-menu');
-            if (submenu) {
-                submenu.style.visibility = 'hidden';
-                submenu.style.maxHeight = '0';
-            }
+
+        // Esconde o submenu ao retirar o mouse do item
+        dropdown.addEventListener('mouseout', function() {
+            this.querySelector('.sub-menu').style.visibility = 'hidden';
+            this.querySelector('.sub-menu').style.maxHeight = '0';
         });
     });
 }
