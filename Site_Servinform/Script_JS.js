78e70ee8923f9467c42d4bbba4bf3ef71f4892cd
José Soares <janyelrodrigues@hotmail.com>
2024-07-28 14:29:03 +0100

    alterei tudo como era antes

--- Site_Servinform/Script_JS.js
+++ Site_Servinform/Script_JS.js
@@ -1,6 +1,10 @@
+// Aguarda até que o DOM esteja totalmente carregado
 document.addEventListener('DOMContentLoaded', function() {
+    // Seleciona o botão de alternância de cor pelo ID
     const colorToggleBtn = document.getElementById('colorToggle');
+    // Adiciona um evento de clique ao botão
     colorToggleBtn.addEventListener('click', function() {
+        // Alterna a classe 'blue-bg' no corpo do documento
         document.body.classList.toggle('blue-bg');
     });
 });
