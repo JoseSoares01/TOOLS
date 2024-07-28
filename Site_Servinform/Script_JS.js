e4b985923f6c479af43da0cd298ccbe70f43e481
José Soares <janyelrodrigues@hotmail.com>
2024-07-28 14:17:48 +0100

    alteração na estrtura do codigo principal, no arquivo css e no java

--- Site_Servinform/Script_JS.js
+++ Site_Servinform/Script_JS.js
@@ -1,23 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    Script_Js(); // Chama a função Script_Js quando o DOM está completamente carregado
-});
-
-function Script_Js() {
-    // Seleciona todos os itens de menu que possuem submenus
-    const dropdowns = document.querySelectorAll('nav.menu .menu-item');
-
-    // Adiciona eventos de mouseover e mouseout para cada item do menu
-    dropdowns.forEach(dropdown => {
-        // Mostra o submenu ao passar o mouse sobre o item
-        dropdown.addEventListener('mouseover', function() {
-            this.querySelector('.sub-menu').style.visibility = 'visible';
-            this.querySelector('.sub-menu').style.maxHeight = '12rem';
-        });
-
-        // Esconde o submenu ao retirar o mouse do item
-        dropdown.addEventListener('mouseout', function() {
-            this.querySelector('.sub-menu').style.visibility = 'hidden';
-            this.querySelector('.sub-menu').style.maxHeight = '0';
-        });
+    const colorToggleBtn = document.getElementById('colorToggle');
+    colorToggleBtn.addEventListener('click', function() {
+        document.body.classList.toggle('blue-bg');
     });
-}
+});
